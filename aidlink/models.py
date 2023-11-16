e21795b4bfdb900c1af51ac6dbec6c80a550cb3e
dlghkdud <dlflo00f@gmail.com>
2023-11-16 23:50:46 +0900

    지도api 연결, 메인에 지도, 네비바 표시

--- aidlink/models.py
+++ aidlink/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Main(models.Model):
+    create_date = models.DateTimeField()
--- aidlink/views.py
+++ aidlink/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Main
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    main = Main.objects.order_by('-create_date')
+    context = {'main':main}
+    return render(request, '../templates/main.html',context)
